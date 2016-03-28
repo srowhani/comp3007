7c4796cebb68080727c4a13061c10f4ea874c669
Seena Rowhani <srowhani@ciena.com>
2016-03-28 15:05:59 -0400

    q4,q5

--- a4/a4_q4.pl
+++ a4/a4_q4.pl
@@ -1,7 +1,16 @@
 % [3 marks] Write a predicate to find the last element of a list. You may not use the built in last predicate in your answer.
-
+mylast(X, [X]).
+mylast(X, [ _ | Y ]) :- mylast(X, Y), !.
 % [3 marks] Write a predicate after(X,List,Result) that returns everything in a list after any occurrence of the given element
+after(X, [X | Y], R) :- R = Y, !.
+after(Query, [Current | Rest], Result) :-
+  after(Query, Rest, Result).
 
 % [4 marks] Write a predicate nextto(X, Y, L), that succeeds when elements X and Y are immediately consecutive elements of a list L.
-
-% [5 marks] Write the predicate occurs_at_position(Element, List, Position), that allows access to the nth element in a list. 
+nextto(X,Y,[X,Y|R]).
+nextto(X,Y,[_|R]) :- nextto(X,Y,R).
+% [5 marks] Write the predicate occurs_at_position(Element, List, Position), that allows access to the nth element in a list.
+occurs_at_position(E,[X | Y],0) :- E = X.
+occurs_at_position(E, [X | Y], I) :-
+  I_1 is I-1,
+  occurs_at_position(E, Y, I_1).
--- a4/a4_q5.pl
+++ a4/a4_q5.pl
@@ -1,4 +1,15 @@
 % [2 marks] Write a predicate myAppend(L1,L2,L3) to append 2 lists (order is important, you may not use the built in append)
+myAppend([],X,X).
+myAppend([X|Y],Z,[X|W]) :-
+  myAppend(Y,Z,W).
 % [2 marks] Rewrite the predicate myLast(X,L) from the previous question using append.
+myLast( X, L ) :-
+  append(_, [X], L).
 % [3 marks] Rewrite the predicate nextto(X,Y,L) from the previous question using append.
+nextto( X, Y, L ) :-
+  append(_, [_|X,Y|_], L).
 % [3 marks] Write a predicate myReverse(L1,L2) that succeeds when the result of reversing the elements of list L1 is the list L2. Your answer should use append, and may not make use of the built in reverse predicate.
+myReverse([], L2) :- writeln()
+myReverse([X|Y],L2) :-
+  append(L2, X, L3).
+  myReverse(Y, L3).
